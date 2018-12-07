8d55aca2fe98c19639f2c4a854c31573a2ea3587
Michelangelo Conserva <michelangelo.conserva@protonmail.com>
2018-12-07 13:04:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='dlgoTutorial',
-      version='0.1',
-     )
+try:
+    with open('LICENSE.txt', 'r') as f:
+        _license = f.read()
+except:
+    _license = ''
+
+try:
+    with open('README.md', 'r') as f:
+        _readme = f.read()
+except:
+    _readme = ''
+
+setup(
+  name='financePy',
+  packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+  version='0.1',
+  description='',
+  author='Michelangelo Conserva',
+  author_email='michelangelo.conserva@protonmail.com',
+  license=_license,
+  long_description=_readme
+)
